932caf299096cbb341c00f30399585f64638f642
Nahuel Lezcano <nlezcano.dev@gmail.com>
2024-01-20 19:45:20 -0300

    First commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ['plugin:vue/essential', 'eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        // Personaliza las reglas según tus necesidades
+    },
+}
